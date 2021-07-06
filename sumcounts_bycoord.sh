7b701bfa11edda154a9999ae0c2e05c4026ab9f5
Zian Liu <56332892+ZnL-BCM@users.noreply.github.com>
2021-07-06 17:27:55 -0500

    Add files via upload

---
+++ sumcounts_bycoord.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Sorts and combines read files based on common coordinates
+
+Out=$1
+In=$2
+echo "Input files: "$In
+echo "Output file: "$Out
+
+# The first three lines combines, uncomments, 
+# amd sorts the input data.
+# then, notice that IF fields 1 and 2 are duplicated, 
+# the script will add up fields 5, 6, and 7, 
+# and then re-calculate field 8 which is a division.
+# This is meant for a bisulfite sequencing read file (from Bismark) FYI
+
+cat $In |\
+  grep -v "#" |\
+  sort -k1,1 -k2,2n |\
+  awk -v OFS="\t" 'BEGIN{ FS = OFS = "\t" }
+    NR > 1{
+      if (($1 == f1) && ($2 == f2)) {
+        $5 += f5; $6 += f6; $7 += f7; $8 = ($6/$5)
+      } else { print rec }
+    }
+    {
+      rec = $0; f1 = $1; f2 = $2; f5 = $5; f6 = $6; f7 = $7
+    }
+    END{ print rec }' > $Out
+
+echo "Done."
